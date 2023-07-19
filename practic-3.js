da244b1f2fb50cceb4801c7ea465109e35e08a1f
mahnaz-aliasghari <MahnazAsghari@gmai.com>
2023-07-19 17:31:44 +0330

    push js

---
+++ practic-3.js
@@ -0,0 +1,11 @@
+let w= parseInt( prompt("enter any number for width"));
+let l=parseInt( prompt("enter any number for length"));
+
+if(w==l){
+    let result=w*l
+    console.log("area" + result)
+}
+else{
+    let result= (w+l)*2;
+   console.log ("Environment"+result)
+}
